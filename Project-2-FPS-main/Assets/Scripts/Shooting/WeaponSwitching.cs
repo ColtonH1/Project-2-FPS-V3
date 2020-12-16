941cdb12630849eae29add6163f43445419110cc
Colton Henderson <coltonhenderson12@hotmail.com>
2020-12-16 13:53:56 -0600

    Added Mini Map

--- Project-2-FPS-main/Assets/Scripts/Shooting/WeaponSwitching.cs
+++ Project-2-FPS-main/Assets/Scripts/Shooting/WeaponSwitching.cs
@@ -35,7 +35,17 @@ public class WeaponSwitching : MonoBehaviour
                 selectedWeapon--;
         }
 
-        if(previousSelectedWeapon != selectedWeapon)
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            selectedWeapon = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2) && transform.childCount >= 2)
+        {
+            selectedWeapon = 1;
+        }
+
+        if (previousSelectedWeapon != selectedWeapon)
         {
             SelectWeapon();
         }
